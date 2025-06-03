4397b4c11ef6cc6606d2fe1084dbd2362f1f09b8
Maarten Van Steenkiste <maarten.vansteenkiste@vlaanderen.be>
2025-06-03 13:46:07 +0200

    Update easy-checkout formula to version 1.2.3 with updated SHA256 checksums for both ARM and AMD architectures.

--- Formula/easy-checkout.rb
+++ Formula/easy-checkout.rb
@@ -1,15 +1,15 @@
 class EasyCheckout < Formula
   desc "CLI tool for managing git branches and checkouts"
   homepage "https://github.com/m44rten1/easy-checkout"
-  version "1.2.2"
+  version "1.2.3"
   license "MIT"
 
   if Hardware::CPU.arm?
     url "https://github.com/m44rten1/easy-checkout/releases/download/v#{version}/easy-checkout-v#{version}-darwin-arm64"
-    sha256 "11dee0e4a40c2d3726ab8d6ec26a457542cc27cb9c9a8ae03d9a038682c8b459"
+    sha256 "8355c5571d14cb6038c4312f5b722f5e3388c0395825942a48eeb82348709130"
   else
     url "https://github.com/m44rten1/easy-checkout/releases/download/v#{version}/easy-checkout-v#{version}-darwin-amd64"
-    sha256 "971c214ce200106e23dd6c900ab4875bdf8bf1813b3a0f4eb36e67abf6b46a5c"
+    sha256 "b2c221c5f6ab2221896c1f604f6a499cb4c2d957bccc21468f5769dce1f18b06"
   end
 
   def install
